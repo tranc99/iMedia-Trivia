a594de56a7b5582569a515cd6c0ffa53f9cb8ac9
Ten Mutunhire <tmutunhire@gmail.com>
2015-08-04 18:55:09 +0300

    props

---
+++ Hello/DetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsViewController.swift
+//  Hello
+//
+//  Created by Ten Mutunhire on 8/4/15.
+//  Copyright (c) 2015 Lean Startup Space Launcher. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    var album: Album?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
